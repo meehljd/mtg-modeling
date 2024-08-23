ffcfe1f7f320452ebf3912a8c9d8e82c3ae0d3a7
Josh Meehl <joshua@meehl.org>
2024-08-23 12:16:01 -0500

    dev(scripts): add notebook header script to change cwd, load core libraries.

---
+++ scripts/notebook_header.py
@@ -0,0 +1,17 @@
+# This helper script is run as a header in the Jupyter Notebooks.
+
+import sys
+import os
+from pathlib import Path
+
+# Define project root
+project_root = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
+
+# Change the working directory to the project root
+os.chdir(project_root)
+print(f"Changed working directory to: {project_root}")
+
+# Autoreload modules
+if 'IPython.extensions.autoreload' not in get_ipython().extension_manager.loaded:
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')

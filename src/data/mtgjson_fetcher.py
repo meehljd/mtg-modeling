669ec5f27c0cc50bb02a8c2d7652b9ad1cb71cd9
Josh Meehl <joshua@meehl.org>
2024-08-24 20:58:56 -0500

    bug: fix unzipping of gz files.  Formatting improvements.

--- src/data/mtgjson_fetcher.py
+++ src/data/mtgjson_fetcher.py
@@ -1,29 +1,26 @@
 """ Fetches MTG JSON data from API """
 
 import os
-import json
 import platform
 from pathlib import Path
 from datetime import datetime
 
 
 class MtgJsonFetcher:
-    def __init__(self, dataset: str, save_root:str='data/raw/mtgjson'):
+    
+    def __init__(self, dataset: str, save_root: str = "data/raw/mtgjson"):
         self.dataset = dataset
         self.save_root = Path(save_root)
         self.final_path = self.save_root / Path(self.dataset).stem
         self._validate_inputs()
-        
+
         self.os_name = platform.system()
         self.filename = self._get_filename()
         self.filepath = self.save_root / self.filename
         self.api_url = f"https://mtgjson.com/api/v5/{self.filename}"
 
-        # filepath = save_path / file
-        # final_path = save_root / file_base.stem
-
     def fetch(self):
-        """ Fetches the data from the URL and decompresses. """
+        """Fetches the data from the URL and decompresses."""
 
         print(f"Downloading {self.dataset} Data")
         print(f"Starting datetime: {datetime.now()}")
@@ -42,45 +39,55 @@ class MtgJsonFetcher:
         print(f"Finished datetime: {datetime.now()}")
 
     def _validate_inputs(self):
-        """ Validates the inputs for the fetcher """
+        """Validates the inputs for the fetcher"""
 
         # Validate dataset
-        valid_datasets = ['AllPrices.json', 'AllPrintingsParquetFiles']
+        valid_datasets = ["AllPrices.json", "AllPrintingsParquetFiles"]
         if self.dataset not in valid_datasets:
             raise ValueError(f"Dataset must be one of {valid_datasets}.")
 
         # Make directory if it doesn't exist
         os.makedirs(self.save_root, exist_ok=True)
         os.makedirs(self.final_path, exist_ok=True)
-    
+
     def _get_filename(self):
-        """ Gets the filename for the dataset """
+        """Gets the filename for the dataset"""
 
         if self.os_name == "Linux":
-            if self.dataset == 'AllPrintingsParquetFiles':
-                file_ext = 'tar.gz'
+            if self.dataset == "AllPrintingsParquetFiles":
+                file_ext = "tar.gz"
             else:
-                file_ext = 'gz'
+                file_ext = "gz"
         elif self.os_name == "Windows":
-            file_ext = 'zip'
+            file_ext = "zip"
         else:
             raise ValueError(f"OS {self.os_name} not supported.")
 
         return f"{self.dataset}.{file_ext}"
 
     def _linux_data_fetch(self):
-        """ Linux commands """
+        """Linux commands"""
         os.system(f"wget -P {self.save_root} --progress=dot:giga {self.api_url}")
-        os.system(f"tar -xzf {self.filepath} -C {self.save_root}")
+        if "tar" in self.filepath.name:
+            os.system(f"tar -xzf {self.filepath} -C {self.save_root}")
+        else:
+            os.system(f"gunzip -c  {self.filepath} > {self.final_path / self.dataset}")
         os.system(f"rm {self.filepath}")
         size = os.system(f"du -sh {self.save_root}")
         return size
 
     def _windows_data_fetch(self):
-        """ Windows commands (using PowerShell) """
-        os.system(f"powershell Invoke-WebRequest -Uri {self.api_url} -OutFile {self.filepath}")
-        os.system(f"powershell Expand-Archive -Path {self.filepath} -DestinationPath {self.final_path}")
+        """Windows commands (using PowerShell)"""
+        os.system(
+            f"powershell Invoke-WebRequest -Uri {self.api_url} -OutFile {self.filepath}"
+        )
+        os.system(
+            f"powershell Expand-Archive -Path {self.filepath} -DestinationPath {self.final_path}"
+        )
         os.system(f"powershell Remove-Item {self.filepath}")
-        size = os.system(f"powershell Get-ChildItem {self.save_root} | Measure-Object -Property Length -Sum | ForEach-Object {{ $_.Sum / 1MB }}")
+        size = os.system(
+            f"powershell Get-ChildItem {self.save_root} | "
+            f"Measure-Object -Property Length -Sum | "
+            f"ForEach-Object {{ $_.Sum / 1MB }}"
+        )
         return size
-    
